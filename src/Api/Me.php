ca5254f4a3d3efc2c713fe0ca38ad8475f125e33
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-10-20 11:43:31 +0600

    fix: unnecessary comment removed from Api/Me

--- src/Api/Me.php
+++ src/Api/Me.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Nahid\EnvatoPHP\Api;
 
@@ -11,27 +11,26 @@ class Me extends AbstractApi
         $this->config = $config;
         parent::__construct();
     }
-   
+
     public function accounts()
     {
-        return $this->get('/v1/market/private/user/account.json');    
-    } 
+        return $this->get('/v1/market/private/user/account.json');
+    }
 
     public function sales($page = 1)
     {
-         //->headers(['Authorization'=> 'Bearer ' . $this->personalToken])
         return $this->get('/v3/market/author/sales?page=' . $page);
     }
 
     public function sale($code)
     {
-        return $this->get('/v3/market/author/sale?code=' . $code); 
+        return $this->get('/v3/market/author/sale?code=' . $code);
     }
 
     public function username()
     {
         return $this->get('/v1/market/private/user/username.json');
-    }  
+    }
 
     public function email()
     {
@@ -39,7 +38,7 @@ class Me extends AbstractApi
     }
 
     public function earningAndSalesByMonth()
-    {           
+    {
         return $this->get('/v1/market/private/user/earnings-and-sales-by-month.json');
     }
 
@@ -70,17 +69,17 @@ class Me extends AbstractApi
         $args .= !is_null($toDate)?'&to_date=' . $toDate:'';
         $args .= !is_null($type)?'&type=' . $type:'';
         $args .= !is_null($site)?'&site=' . $site:'';
-    
+
         return $this->get('/v3/market/user/statement?' . $args);
     }
 
      public function bookmarks()
      {
         return $this->get('/v3/market/user/bookmarks');
-     } 
+     }
 
      public function collection($id)
      {
         return $this->get('/v3/market/user/collection?id=' . $id);
-     } 
+     }
 }
